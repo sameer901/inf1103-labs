9b14ad808c3f5fce974bb5bcdf6d57d57aafd5a0
Sameerudeen <2601549@sit.singaporetech.edu.sg>
2026-09-11 14:56:25 +0800

    Third Post : Follower

--- hello.py
+++ hello.py
@@ -7,6 +7,15 @@ username = "cool_creator "
 bio = "Fun Vlogger"
 Followers = 100
 
+Followers += 50
+print("Day 1: ", Followers)
+
+Followers += 20
+print("Day 2: ", Followers)
+
+Followers += 10
+print("Day 3: ", Followers)
+
 print("Username: ", username)
 print("Bio: ", bio )
 print("Followers: ", Followers)

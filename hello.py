626ca43703add2e37b25732b6e98582d2585402a
Sameerudeen <2601549@sit.singaporetech.edu.sg>
2026-09-11 14:51:24 +0800

    Second Post : Update Profile

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("========")
 print("Welcome here")
 print("My First Post!")
 print("========")
+
+username = "cool_creator "
+bio = "Fun Vlogger"
+Followers = 100
+
+print("Username: ", username)
+print("Bio: ", bio )
+print("Followers: ", Followers)

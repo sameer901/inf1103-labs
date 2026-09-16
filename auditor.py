73180d29c29702c7d3cc5f5067a962464af6d412
Sameerudeen <2601549@sit.singaporetech.edu.sg>
2026-09-16 12:13:32 +0800

    update auditor

---
+++ auditor.py
@@ -0,0 +1,15 @@
+inventory = 0
+Stock_Qty = 0
+
+#while Stock_Qty == 0:
+#    stock = input("Enter Stock Value or enter 'quit' to exit ")
+#    if Stock_Qty == "quit":
+#        break
+#    elif :
+    
+#    elif int(stock) :
+#        if stock < 0 :
+#            print("Enter a positive Value")
+#        else :
+    #input("Enter Stock  Value") = Stock_Qty
+    #p#rint

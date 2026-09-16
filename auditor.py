995ed4cdbcf99dcc479fe2d33345f7361df1f55c
Sameerudeen <2601549@sit.singaporetech.edu.sg>
2026-09-16 13:58:03 +0800

    update code , change order of validation

--- auditor.py
+++ auditor.py
@@ -1,26 +1,24 @@
 inventory = 0
-Stock_Qty = 0
 failed_entries = 0
 
 while True :
     stock = input("enter Stock Value or enter quit to exit")
     if stock == "quit":
         break
-    elif stock.isdigit() == False:
-        print("Plese input a valid Integer number")
-        failed_entries += 1
-        #print(failed_entries)
-        continue
-
     elif stock.startswith("-") and stock[1:].isdigit() :
             print("This a negative number, please input a valid number")
             failed_entries += 1
+            continue
+    elif stock.isdigit() == False:
+            print("Plese input a valid Integer number")
+            failed_entries += 1
+            #print(failed_entries)
             continue
     
     else: 
         stock = int(stock)
         inventory += stock 
-        print("You total inventory is " + stock)
+        print("You total inventory is " + str(inventory))
 
 
 #while Stock_Qty == 0:
